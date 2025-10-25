6c636019a7b4df875741c8b2daf6518637e7659a
TheGod7 <br.iancefi12@gmail.com>
2025-10-25 17:03:13 -0600

    Replace IP with localhost/default env variable

--- constants/config.tsx
+++ constants/config.tsx
@@ -3,7 +3,7 @@ import Constants from "expo-constants";
 const extra = Constants.expoConfig?.extra ?? {};
 
 const Config = {
-  baseUrl: "extra\.EXPO_PUBLIC_API_BASE_URL || \"http:\/\/localhost:3000\/\"",
+  baseUrl: extra.EXPO_PUBLIC_API_BASE_URL || "http://localhost:3000/",
   googleWebClientId: extra.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID ?? "",
   googleIosClientId: extra.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID ?? "",
 };
